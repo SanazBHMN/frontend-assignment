625545b379001370f0489dfbc73b94f7a2618604
SanazBHMN <sanazbahmani35@gmail.com>
2023-03-23 11:46:31 +0330

    Implement the PoemList

---
+++ src/components/PoemList.jsx
@@ -0,0 +1,15 @@
+import PoemCard from "./PoemCard";
+
+const PoemList = ({ poems }) => {
+  return (
+    <>
+      {/* {poems.map((poem) => (
+        <div>
+          <PoemCard title={poem.title} author={poem.author} />
+        </div>
+      ))} */}
+    </>
+  );
+};
+
+export default PoemList;
