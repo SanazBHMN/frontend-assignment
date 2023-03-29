f5b846ec06b9c7588bf192d406128237c14ef71f
SanazBHMN <sanazbahmani35@gmail.com>
2023-03-29 12:42:51 +0330

    Display title and author of fetched poems

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [poems, setPoems] = useState();
+
+  function fetchPoems() {
+    fetch("https://poetrydb.org/random/20")
+      .then((response) => response.json())
+      .then((data) => {
+        setPoems(data);
+        console.log(data);
+      });
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={fetchPoems}>Fetch Poems</button>
+      {poems &&
+        poems.map((poem, index) => {
+          return (
+            <div>
+              <small>{index + 1}</small>
+              <h3>{poem.title}</h3>
+              <span>{poem.author}</span>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
